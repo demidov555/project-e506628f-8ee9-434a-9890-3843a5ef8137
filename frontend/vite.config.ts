c4f438740a3d0e78d501fe316c5111af548ddffc
demidov555 <miha.777vip@mail.ru>
2025-12-09 18:06:21 +0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'project-GwoVMP',
+  base: 'project-e506628f-8ee9-434a-9890-3843a5ef8137',
   plugins: [react()]
 });
