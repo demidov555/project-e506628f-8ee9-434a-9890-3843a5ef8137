6beeeb9fac5fe640bf97a3fe44a36af4e1fbf488
demidov555 <miha.777vip@mail.ru>
2025-12-12 19:32:42 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,17 @@
-def main():
-    print('Hello world')
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes import projects, messages, auth, agents
+from dotenv import load_dotenv
 
+load_dotenv()
 
-if __name__ == '__main__':
-    main()
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
+
+app.include_router(projects.router)
